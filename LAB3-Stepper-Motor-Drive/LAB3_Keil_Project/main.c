728c802db5247652975a6dc60f7a808b686a94ce
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2022-11-27 23:33:58 +0300

    LAB3 - Q3 w/o debounce

--- LAB3-Stepper-Motor-Drive/LAB3_Keil_Project/main.c
+++ LAB3-Stepper-Motor-Drive/LAB3_Keil_Project/main.c
@@ -9,10 +9,15 @@ void init_gpio(void);
 void init_systick(void);
 void SysTick_Handler(void);
 
-uint32_t restart = 0; 
+uint32_t down_restart = 0; 
+uint32_t up_restart = 0;
+
+int step_up = 0;
+int	step_down = 0;
+
 void init_systick(void){
 	
-	SysTick->LOAD = 99999; /* 1 second reload value (16MHz) */
+	SysTick->LOAD = 999999; /* 1 second reload value (16MHz) */
 	SysTick->CTRL = 7; /* (0x07) Enable counter, interrupt and select system bus clock */
 	SysTick->VAL = 0; /* Initialize current value register */
 }
@@ -29,9 +34,10 @@ void init_gpio(void){
 	GPIOF->DEN |= 0x08;				/* Enable digital mode */
 }
 
-
+/*/////////////////////PART-1///////////////////////////*/
+/*
 void SysTick_Handler(void) {
-	/*count = count +1;*/
+	//count = count +1;
 	if (restart == 1){
 		restart = 0; 
 		GPIOB->DATA = 1;
@@ -45,7 +51,7 @@ void SysTick_Handler(void) {
 		restart = 1; 
 	}
 
-	GPIOF->DATA = GPIOF->DATA ^ 8 ; /* For debugging purposes */
+	GPIOF->DATA = GPIOF->DATA ^ 8 ; // For debugging purposes 
 }
 
 int main(void) {
@@ -54,4 +60,79 @@ int main(void) {
 	GPIOB->DATA = 1;
 	while(1){
 	}
+}  
+*/
+/*/////////////////////PART-1///////////////////////////*/
+
+
+
+
+/*/////////////////////PART-2///////////////////////////*/
+
+
+
+void SysTick_Handler(void) {
+
+	int pin_state = (0xF0 & GPIOB->DATA) >> 4;
+	if (pin_state == 14){  // d14 = b1110
+		//__ASM("BL DELAY50");
+		//__ASM("BL DELAY50");
+		if (pin_state == 14){ // d14 = b1110
+		step_up = 1;
+		GPIOF->DATA = GPIOF->DATA ^ 8 ; // For debugging purposes 
+		}
+	}
+	
+	if (pin_state == 13){  // d13 = b1101
+	//__ASM("BL DELAY50");
+		if (pin_state == 13){ // d13 = b1101
+		step_down = 1;
+		}else{
+		step_down = 0;		
+		}
+	}
+		if (pin_state == 15){ // d15 = b1111
+		//__ASM("BL DELAY50");
+			if (pin_state == 15){ // d15 = b1111
+			if (step_up == 1){
+					step_up = 0;
+					if (up_restart == 1){
+					up_restart = 0; 
+					GPIOB->DATA = (GPIOB->DATA & 0xF0) +  0x01;
+					}
+					else{
+					GPIOB->DATA = (GPIOB->DATA & 0xF0) + ((GPIOB->DATA*2)& 0x0F) ;
+					}
+					if ((GPIOB->DATA & 0x0F) == 8){
+					up_restart = 1; 
+					}	
+					GPIOF->DATA = GPIOF->DATA ^ 8 ;
+		}
+			
+			if (step_down == 1){
+					step_down = 0;
+					if (down_restart == 1){
+					down_restart = 0; 
+					GPIOB->DATA = (GPIOB->DATA & 0xF0) +  0x08;
+					}
+					else{
+					GPIOB->DATA = (GPIOB->DATA & 0xF0) + ((GPIOB->DATA& 0x0F)/2) ;
+					}
+					if ((GPIOB->DATA & 0x0F) ==  1){
+					down_restart = 1; 
+					}	
+			}
+		
+		}
+	}
 }
+
+int main(void) {
+	init_gpio();
+	init_systick();
+	GPIOB->DATA = 1;
+	while(1){
+	}
+}  
+
+/*/////////////////////PART-2///////////////////////////*/
