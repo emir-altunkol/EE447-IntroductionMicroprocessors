334f6c0fcb38838a30b01fa05719384a6c1a0c76
unknown <emir.altunkol01@gmail.com>
2022-12-04 20:07:55 +0300

    Q2 with interrupt successful

--- LAB4-General-Purpose-Timers/LAB4_Keil_Project/main2interrupt.c
+++ LAB4-General-Purpose-Timers/LAB4_Keil_Project/main2interrupt.c
@@ -17,7 +17,7 @@ void Delay(unsigned long counter);
 
 
 /* Message for OutStr. Note the 0x04 = \4 termination character. */
-char msg[80];
+char msg[100];
 
 /* Reference external subroutine with full signature */
 /* OutStr takes a pointer (address) as an  */
@@ -29,16 +29,19 @@ extern void OutStr(char*);
 #define HIGH 	40
 
 int count = 0;
-double time_period;
-double time_width;
-double time_duty;
+volatile double time_period;      // Global variables that used in ISR should be volatile
+volatile double time_width;				// Global variables that used in ISR should be volatile
+volatile double time_duty;				// Global variables that used in ISR should be volatile
 uint32_t rising_edge_current = 0;
 uint32_t rising_edge_former  = 0;
 uint32_t falling_edge = 0;
 
 void pulse_init(void){
 	volatile int *NVIC_EN0 = (volatile int*) 0xE000E100;
+	volatile int *NVIC_EN1 = (volatile int*) 0xE000E104;
 	volatile int *NVIC_PRI4 = (volatile int*) 0xE000E410;
+	volatile int *NVIC_PRI8 = (volatile int*) 0xE000E420;
+
 	SYSCTL->RCGCGPIO |= 0x20; // turn on bus clock for GPIOF
 	__ASM("NOP");
 	__ASM("NOP");
@@ -94,16 +97,22 @@ void pulse_init(void){
 	//Timer0A is interrupt 19
 	//Interrupt 16-19 are handled by NVIC register PRI4
 	//Interrupt 19 is controlled by bits 31:29 of PRI4
+	//Interrupt 35 is controlled by bits 31:29 of PRI8
+
 	*NVIC_PRI4 &=0x00FFFFFF; //Clear interrupt 19 priority
 	*NVIC_PRI4 |=0x40000000; //Set interrupt 19 priority to 2
-	*NVIC_PRI4 |=0x00004000; //Set interrupt 19 priority to 2
+	
+	*NVIC_PRI8 &=0x00FFFFFF; //Clear interrupt 35 priority
+	*NVIC_PRI8 |=0x60000000; //Set interrupt 35 priority to 3
+
+	//*NVIC_PRI4 |=0x00004000; //Set interrupt 35 priority to 2
 	
 	//NVIC has to be neabled
 	//Interrupts 0-31 are handled by NVIC register EN0
 	//Interrupt 19 is controlled by bit 19
 	*NVIC_EN0 |=0x00080000;
-	//Interrupt 19 is controlled by bit 19
-	*NVIC_EN0 |=0x00020000;
+	//Interrupt 35 is controlled by bit 35
+	*NVIC_EN1 |=0x00000008;
 	
 	//Enable timer
 	TIMER0->CTL			 |=0x03; // bit0 to enable and bit 1 to stall on debug
@@ -123,7 +132,6 @@ void TIMER0A_Handler (void){
 	}
 	TIMER0->ICR |=0x01; //Clear the interrupt
 	
-	return;
 }
 
 void TIMER3A_Handler (void){
@@ -133,14 +141,14 @@ void TIMER3A_Handler (void){
 	{
 		rising_edge_current = TIMER3->TAR;     /* save the timestamp */
 		if (rising_edge_current > rising_edge_former){
-			time_period = rising_edge_current - rising_edge_former;
-		}
+			time_period = (((rising_edge_current - rising_edge_former) & 0x00FFFFFF )* 62.5)/1000; 
+	}
 	}
 	else /*if falling edge occurs */
 	{	
 		falling_edge = TIMER3->TAR;     /* save the timestamp */
 		if (falling_edge > rising_edge_former){
-			time_width = falling_edge - rising_edge_former;
+			time_width = (((falling_edge - rising_edge_former) & 0x00FFFFFF )* 62.5)/1000; 
 		}
 	}
 	
@@ -202,9 +210,12 @@ int main (void){
 	while(1){
 		// time_period = Timer3ACapture_Period();
 		// time_width = Timer3ACapture_PulseWidth();
+		TIMER3->IMR			=0x0; //Disable capture mode event interrupt before accessing the global variables which are used in ISR
 		time_duty =  time_width/time_period;
 		sprintf(msg,"\r\n %0.2f us, %0.2f us,  %0.2f %% \r\4", time_width, time_period, time_duty);
 		OutStr(msg);
-		Delay(1000);
+		TIMER3->IMR			=0x4; //Enable capture mode event interrupt	
+
+		Delay(500);
 	}
 }
