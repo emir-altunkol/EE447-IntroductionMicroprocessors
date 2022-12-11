1a00c359ebebe07baaf6fa4deac249bf6ffd0b30
Özgür Gülsuna <ozgurgulsuna@gmail.com>
2022-12-11 16:01:18 +0300

    LAB5-Initial "ATD" code

---
+++ LAB5-Analog-to-Digital-Converter/LAB5_Keil_Project/Objects/adc.d
@@ -0,0 +1,9 @@
+./objects/adc.o: ADC.c \
+  C:\Users\ozgur\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\TM4C123GH6PM.h \
+  C:\Users\ozgur\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\ozgur\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\ozgur\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\ozgur\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\ozgur\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\ozgur\AppData\Local\Arm\Packs\Keil\TM4C_DFP\1.1.0\Device\Include\TM4C123\system_TM4C123.h
